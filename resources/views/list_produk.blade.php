9ed08fff0c4cb8a38da020b8f9fba3799d089ad9
Naylah013 <naylahamirah13@gmail.com>
2025-03-15 22:59:43 +0700

    menambahkan view list_produk

---
+++ resources/views/list_produk.blade.php
@@ -0,0 +1,8 @@
+<html>
+    <div>
+        <h1>List Produk</h1>
+        <p>Kode Produk: {{$id}}</p>
+        <p>Nama Produk: {{$nama}}</p>
+        <p>Harga Produk: {{$harga}}</p>
+    </div>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/listproduk/{id}/{nama}/{harga}', function ($id, $nama, $harga) {
+    return view('list_produk', compact('id', 'nama', 'harga'));
+});
